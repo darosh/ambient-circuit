dec4e76506d023b9f8acd9565a6f7c50fe36a86d
Jan Forst <ensonador@gmail.com>
2026-02-07 20:26:22 +0100

    vite build for webgpu

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ export default defineConfig({
 	plugins: [svelte()],
 	base: './',
 	build: {
+		target: 'esnext',
 		outDir: './docs'
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: 'esnext'
+		}
+	},
 })
