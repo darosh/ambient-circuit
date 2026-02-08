bedbfe78af17a154c745051bfbf703846a878bf8
Jan Forst <ensonador@gmail.com>
2026-02-08 11:47:47 +0100

    material config

---
+++ src/lib/config.ts
@@ -0,0 +1,11 @@
+import { createRailMaterial } from './material-rail'
+import { createMarbleMaterial } from './material-marble'
+import { createInstrumentMaterial } from './material-instrument'
+
+export const makeRailMaterial = createRailMaterial
+export const makeMarbleMaterial = createMarbleMaterial
+export const makeInstrumentMaterial = createInstrumentMaterial
+
+// export const makeRailMaterial = createMarbleMaterial
+// export const makeMarbleMaterial = createMarbleMaterial
+// export const makeInstrumentMaterial = createMarbleMaterial
