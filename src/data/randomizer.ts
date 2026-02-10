b24fdc7a8c64f5ee5806b37f0bd58ca3d8fba669
Jan Forst <ensonador@gmail.com>
2026-02-10 20:40:22 +0100

    scene random structure

---
+++ src/data/randomizer.ts
@@ -0,0 +1,8 @@
+export function randomizer(p = 1) {
+	let previous = p
+
+	return () => {
+		previous = (previous * 16807) % 2147483647
+		return previous / 2147483647
+	}
+}
--- src/data/scene-structure.ts
+++ src/data/scene-structure.ts
@@ -1,14 +1,19 @@
 import type { SceneConfig } from '../lib/scene'
 import { colors } from './colors'
 import { triggerHandler } from '../lib/trigger-handler'
+import { randomizer } from './randomizer'
 
 let ci = 0
 const _c = () => colors[ci++ % colors.length]
+
+const r = randomizer(2)
 
 export const scene: SceneConfig = {
 	id: 'scene-structure',
 	bpm: 120,
 	triggerHandler,
+	camera: [12, 7, -17],
+	target: [0, 2.2, 0],
 	rails: [
 		{
 			color: colors[1],
@@ -20,7 +25,16 @@ export const scene: SceneConfig = {
 					'o1 l1 u1 l1 d1 r1 d1 l1 u1 r1 u1 l1 d1 r1 d1 l1 ',
 					'i1 r1 i1 u1 i1 l1 i1 d1 i1 r1 i1 u1 i1 l1 i1 d1 '
 				]
-			}
+			},
+			marbles: Array.from({ length: 12 }).map(() => ({
+				direction: r() < 0.5 ? 'backward' : 'forward',
+				start: Math.round(r() * 50),
+				speed: Math.floor(r() * 2 + 1)
+			})),
+			instruments: Array.from({ length: 12 }).map(() => ({
+				beat: Math.round(r() * 50) + 0.5,
+				type: 'sun'
+			}))
 		},
 		{
 			color: colors[0],
@@ -32,7 +46,16 @@ export const scene: SceneConfig = {
 					'i2 l2 i1 l1 i1 r2 i1 l1 u1 l2 u1 r1 u1 l1 u1 r1 ',
 					'o1 r1 o1 u1 o1 l1 o1 d1 o1 r1 o1 u1 o1 l1 o1 d1 '
 				]
-			}
+			},
+			marbles: Array.from({ length: 12 }).map(() => ({
+				direction: r() < 0.5 ? 'backward' : 'forward',
+				start: Math.round(r() * 60),
+				speed: Math.floor(r() * 2 + 1)
+			})),
+			instruments: Array.from({ length: 12 }).map(() => ({
+				beat: Math.round(r() * 60) + 0.5,
+				type: 'sun'
+			}))
 		},
 		{
 			color: colors[3],
@@ -48,7 +71,16 @@ export const scene: SceneConfig = {
 					'o2 r2 o1 r1 o1 l2 o1 r1 u1 r2 u1 l1 u1 r1 u1 l1 ',
 					'd1 r1 d1 l1 d1 r1 d1 l1 i1 r1 i1 l1 i1 r1 i1 l1'
 				]
-			}
+			},
+			marbles: Array.from({ length: 12 }).map(() => ({
+				direction: r() < 0.5 ? 'backward' : 'forward',
+				start: Math.round(r() * 110),
+				speed: Math.floor(r() * 2 + 1)
+			})),
+			instruments: Array.from({ length: 12 }).map(() => ({
+				beat: Math.round(r() * 110) + 0.5,
+				type: 'sun'
+			}))
 		}
 	]
 }
