102a666c012458164d797cbeb83c0abec9f2c9eb
Jan Forst <ensonador@gmail.com>
2026-02-17 00:53:05 +0100

    script

--- scripts/tone-defaults.js
+++ scripts/tone-defaults.js
@@ -1,12 +1,30 @@
 import * as Tone from 'tone'
+import { flattenObject } from 'rambda'
 
 import TONE_DEFAULTS from '../src/lib/audio/tone-defaults.ts'
 
-const entries = Object.keys(TONE_DEFAULTS).map((key) => [
-	key,
-	JSON.parse(
-		JSON.stringify({ ...Tone[key].getDefaults(), context: undefined, onsilence: undefined })
-	)
-])
+const entries = Object.keys(TONE_DEFAULTS).map((className) => {
+	const defaults = Tone[className].getDefaults()
+	const flatten = flattenObject(defaults)
+	const filtered = Object.entries(flatten).filter(([key]) => !['context.', 'onsilence', 'voice0.', 'voice1.'].some(fbn => key.startsWith(fbn)))
+	const modifiable = filtered.filter(([key, val]) => {
+		return ![
+			'modulationFrequency',
+			'modulationIndex',
+			'width',
+			'.harmonicity',
+			'partialCount'
+		].some(fu => key.endsWith(fu)) && (typeof val === 'number')
+	})
+	
+	return [
+		className,
+		JSON.parse(
+			JSON.stringify({
+				...Object.fromEntries(modifiable)
+			})
+		)
+	]
+})
 
 console.log(Object.fromEntries(entries))
