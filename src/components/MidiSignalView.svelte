802d58ca7eca30afad75ac07a291ba1ad1415895
Jan Forst <ensonador@gmail.com>
2026-02-18 10:53:20 +0100

    MidiSignalView curved

--- src/components/MidiSignalView.svelte
+++ src/components/MidiSignalView.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import { T, useTask } from '@threlte/core'
 	import { LineCurve3, Color, MeshStandardMaterial, Vector3 } from 'three/webgpu'
-	import type { Vector3Tuple } from 'three'
+	import { CubicBezierCurve3, type Vector3Tuple } from 'three/webgpu'
 	import { easeOutQuart } from '../lib/easing'
+
+	const TUBE_SEGMENTS_STRAIGHT = 1
+	const TUBE_SEGMENTS_CURVED = 32
+	const CURVE_TANGENT = 0.5
+	const NODE_LENGTH = 0.2
+	const LINE_SHIFT = NODE_LENGTH / 2
+	const INS_SHIFT = 0.3
 
 	type SignalLink = {
 		from: Vector3Tuple
@@ -11,7 +18,7 @@
 		color?: string
 	}
 
-	let { links }: { links: SignalLink[] } = $props()
+	let { links, curved = true }: { links: SignalLink[]; curved?: boolean } = $props()
 
 	const DURATION = 0.5,
 		BASE = 0.15,
@@ -19,7 +26,24 @@
 		TUBE_R = 0.02
 
 	const curves = $derived(
-		links.map((l) => new LineCurve3(new Vector3(...l.from), new Vector3(...l.to)))
+		links.map((l) => {
+			const useCurve = curved && true
+			const d = (l.to[1] - l.from[1]) * CURVE_TANGENT
+			return {
+				useCurve,
+				curve: useCurve
+					? new CubicBezierCurve3(
+							new Vector3(l.from[0], l.from[1] + INS_SHIFT, l.from[2]),
+							new Vector3(l.from[0], l.from[1] + d, l.from[2]),
+							new Vector3(l.to[0], l.to[1] - LINE_SHIFT - d, l.to[2]),
+							new Vector3(l.to[0], l.to[1] - LINE_SHIFT, l.to[2])
+						)
+					: new LineCurve3(
+							new Vector3(l.from[0], l.from[1] + INS_SHIFT, l.from[2]),
+							new Vector3(l.to[0], l.to[1] - LINE_SHIFT, l.to[2])
+						)
+			}
+		})
 	)
 	const materials = $derived(
 		links.map(
@@ -55,6 +79,14 @@
 
 {#each links as _, i (i)}
 	<T.Mesh material={materials[i]}>
-		<T.TubeGeometry args={[curves[i], 1, TUBE_R, 4, false]} />
+		<T.TubeGeometry
+			args={[
+				curves[i].curve,
+				curves[i].useCurve ? TUBE_SEGMENTS_CURVED : TUBE_SEGMENTS_STRAIGHT,
+				TUBE_R,
+				4,
+				false
+			]}
+		/>
 	</T.Mesh>
 {/each}
