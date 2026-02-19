bfd4f43c1d23cc41f8b1560030ac1dbd3933fc4a
Jan Forst <ensonador@gmail.com>
2026-02-19 11:52:57 +0100

    vite config chunks

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,18 @@ export default defineConfig({
 	plugins: [svelte()],
 	base: './',
 	build: {
+		chunkSizeWarningLimit: 721, // three
 		target: 'esnext',
 		outDir: './docs',
 		rollupOptions: {
+			onwarn(warning, warn) {
+				if (warning.code === 'EVAL') return
+				warn(warning)
+			},
 			output: {
 				manualChunks: {
+					rnbo: ['@rnbo/js'],
+					tone: ['tone'],
 					three: ['three'],
 					'three-webgpu': ['three/webgpu', 'three/tsl']
 				}
@@ -20,6 +27,7 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}']
 	},
 	optimizeDeps: {
+		exclude: ['@rnbo/js'],
 		include: ['three'],
 		esbuildOptions: {
 			target: 'esnext'
