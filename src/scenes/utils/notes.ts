1b1199c5a4afc70d3013c7662bb240b773ed229a
Jan Forst <ensonador@gmail.com>
2026-02-22 12:18:45 +0100

    lint

--- src/scenes/utils/notes.ts
+++ src/scenes/utils/notes.ts
@@ -1,3 +1,5 @@
 import { tones } from './tones'
 
-export const notes = Object.fromEntries(tones.map((t, i) => [t.replace('-', '_').replace('#', 's'), i])) 
+export const notes = Object.fromEntries(
+	tones.map((t, i) => [t.replace('-', '_').replace('#', 's'), i])
+)
--- src/scenes/utils/tones.ts
+++ src/scenes/utils/tones.ts
@@ -1,3 +1,5 @@
 export const toneNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-export const tones = Array.from({length: 128}).map((_n, i) => `${toneNames[i % 12]}${Math.floor(i / 12) - 1}`)
+export const tones = Array.from({ length: 128 }).map(
+	(_n, i) => `${toneNames[i % 12]}${Math.floor(i / 12) - 1}`
+)
