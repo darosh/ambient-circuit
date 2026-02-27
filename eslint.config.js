b7a7633d1501ae54b08466a2be4f0b5b76e99032
Jan Forst <ensonador@gmail.com>
2026-02-28 00:23:23 +0100

    lint unicorn

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,12 @@ import sonarjs from 'eslint-plugin-sonarjs'
 import svelte from 'eslint-plugin-svelte'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import unicorn from "eslint-plugin-unicorn"
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	sonarjs.configs.recommended,
+	unicorn.configs.recommended,
 	...tseslint.configs.recommended,
 	...svelte.configs['flat/recommended'],
 	prettier,
@@ -39,7 +41,21 @@ export default tseslint.config(
 			'sonarjs/no-nested-conditional': ['off'],
 			'sonarjs/pseudo-random': ['off'],
 			'sonarjs/void-use': ['off'],
-			'sonarjs/todo-tag': ['off']
+			'sonarjs/todo-tag': ['off'],
+			'unicorn/prevent-abbreviations': ['off'],
+			'unicorn/filename-case': ['off'],
+			// Three.js/WebAudio APIs require null (disconnect(null), DOM APIs)
+			'unicorn/no-null': ['off'],
+			// reduce is readable for accumulation patterns
+			'unicorn/no-array-reduce': ['off'],
+			// test helpers intentionally scoped inside describe blocks
+			'unicorn/consistent-function-scoping': ['off'],
+			// passing named fn to .map() is fine
+			'unicorn/no-array-callback-reference': ['off'],
+			// object default params are fine
+			'unicorn/no-object-as-default-parameter': ['off'],
+			// export * is valid syntax
+			'unicorn/require-module-specifiers': ['off']
 		}
 	},
 	{
