81853cad3e9c994ea314c592999e5e8a67853036
Jan Forst <ensonador@gmail.com>
2026-03-16 20:53:33 +0100

    branching order

--- src/scenes/utils/svg-paths.ts
+++ src/scenes/utils/svg-paths.ts
@@ -93,7 +93,7 @@ export const DRAWING_FORMLINES: Record<string, string> = {
 }
 
 export const DRAWING_BRANCHING: Record<string, string> = {
-	r26: 'r3i4 l.75o[1,1](l.75i | o l.75o[1,1](l.75i i2 | o l.75o[1,1](l.75i i4 | o l.75o[1,1](l.75i i6 | o))))'
+	r26: 'o4 i[1,1](r.75i i[1,1](r.75i i[1,1](r.75i i[1,1](r.75i | l.75i) | l.75i i2) | l.75i i4) | l.75i i6)'
 }
 
 export default { DRAWING_CIRCUIT, DRAWING_RAVEN, DRAWING_FORMLINES, DRAWING_BRANCHING }
